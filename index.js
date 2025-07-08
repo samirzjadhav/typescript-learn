e7d63f9074a0d0c4567195fdcd90d742a90137c0
samirzjadhav <samirzjadhav@gmail.com>
2025-07-08 16:48:55 +0530

    init

---
+++ index.js
@@ -0,0 +1,12 @@
+const menu = [
+  { name: "Margherita", price: 8 },
+  { name: "Pepperoni", price: 10 },
+  { name: "Hawaiian", price: 10 },
+  { name: "veggie", price: 9 },
+];
+
+const cashInRegister = 100;
+
+const orderQueue = [];
+
+// Challenge: add utility functions "addNewPizza" that takes a pizza object and adds it to the menu
