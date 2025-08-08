3d652aed184ed7433b93dd67c719f7293a3f7792
samirzjadhav <samirzjadhav@gmail.com>
2025-08-08 15:18:06 +0530

    lean: enumeration

---
+++ app.js
@@ -0,0 +1,14 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var a = 12;
+var b = 13;
+let arr = ["samir", 12];
+// enumeration example
+var UserRoles;
+(function (UserRoles) {
+    UserRoles["ADMIN"] = "admin";
+    UserRoles["GUEST"] = "guest";
+    UserRoles["SUPER_ADMIN"] = "super_admin";
+})(UserRoles || (UserRoles = {}));
+UserRoles.SUPER_ADMIN;
+//# sourceMappingURL=app.js.map
---
+++ app.ts
@@ -0,0 +1,13 @@
+var a = 12;
+var b = 13;
+
+let arr: [string, number] = ["samir", 12];
+
+// enumeration example
+enum UserRoles {
+  ADMIN = "admin",
+  GUEST = "guest",
+  SUPER_ADMIN = "super_admin",
+}
+
+UserRoles.SUPER_ADMIN;
