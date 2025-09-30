8ffac1cc6955ce4aec27d51b1cba0d366c139904
samirzjadhav <samirzjadhav@gmail.com>
2025-09-30 21:45:55 +0530

    feat: interface declaration with classes

--- app.ts
+++ app.ts
@@ -27,4 +27,20 @@ function add(a: number, b: number): number {
 }
 
 console.log(add(n, m));
-  
+
+interface User {
+  name: string;
+  id: number;
+}
+
+class UserAccount {
+  name: string;
+  id: number;
+
+  constructor(name: string, id: number) {
+    this.name = name;
+    this.id = id;
+  }
+}
+
+const user: User = new UserAccount("Murphy", 1);
