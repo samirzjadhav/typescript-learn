08ed3aef7d7059d4577489db1785a17173b84432
samirzjadhav <samirzjadhav@gmail.com>
2026-03-08 07:32:32 +0000

    learn: response using 2 value

--- src/intefaceTs.ts
+++ src/intefaceTs.ts
@@ -38,3 +38,33 @@ class Chai implements CupSize{
   size: 'Small' | 'Large' = 'Large'
 }
 
+
+type Response = {ok: true} | {ok:false}
+class  myRes implements Response {
+    ok: boolean = true
+}
+
+
+type TeaType = 'masala' | 'gingar'| 'leamon';
+
+function orderChai(t: TeaType){
+    console.log(t)
+}
+
+type BaseChai = {teaLeaves: number}
+type Extra = {masala :number}
+
+const MasalaChaiWithMe = BaseChai & Extra
+
+const cup:MasalaChaiWithMe ={
+    teaLeaves =  2,
+    masala: 1
+}
+
+type User = {
+    username: string;
+    bio?:string
+}
+
+const u1: User = {username: 'samir'}
+const u2: User  = {username:'mohit', bio: 'I am youtuber and I love Gaming'}
